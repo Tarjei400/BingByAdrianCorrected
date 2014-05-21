4590a82a70311164a3932f6c75de3a0d14019dd1
Tarjei400 <adrian.jutrowski@gmail.com>
2014-05-21 12:41:28 +0200

    Added Genereted WEKA TrainSet
    Signed-off-by: Tarjei400 <adrian.jutrowski@gmail.com>

--- src/TestSet.java
+++ src/TestSet.java
@@ -1,33 +1,39 @@
 import java.util.*;
+import au.com.bytecode.opencsv.CSVWriter;
+import java.io.*;
+
 public class TestSet {
 	
 	public static void main(String[] arg){
 		try{
 		 JQuestions jq = new JQuestions();
-		 List<QuestionType> randomQuestions = jq.shuffle(100);
+		 List<QuestionType> randomQuestions = jq.shuffle(1000);
 		 int amount = 0;
 		 int bingamount = 0;
-		 int j = 0;
+		
+		 BufferedWriter bf = new BufferedWriter(new FileWriter("TrainSet.csv"));
+		 CSVWriter csvw = new CSVWriter(bf);
 		 for(QuestionType q : randomQuestions){
-			List<SearchEntry> search = LuceneSearch.search(q.getQuestion(), "text", 200);
+			List<SearchEntry> search = LuceneSearch.search(q.getQuestion(), "text", 10);
 
-			List<SearchEntry> validationSet = BingSearch.search(q.getQuestion(), "title", 100);
+			List<SearchEntry> validationSet = BingSearch.search(q.getQuestion(), "title", 10);
 			int BingRank = 0;
-			j++;
+			 int j = 0;
 			for (SearchEntry lse : search){
-			
+				j++;
 				String CA = lse.getTitle().toLowerCase();
-				int bingRank = 0;
+				String bingRank = "?";
 				int i = 0;
 				for (SearchEntry se : validationSet){
 					i++;
 					String bingA = se.getTitle().toLowerCase();
 					//System.out.println("Bing Title:"+bingA+"|| Lucene: "+CA+" Correct: "+q.getAnswer());
 					if (CA.indexOf(bingA) > 0 || bingA.indexOf(CA) > 0){
-						bingRank = i;
+						bingRank = ""+i;
 						break;
 					}
 				}
+				int isTrue = 0;
 				if(CA.indexOf(q.getAnswer().toLowerCase()) > 0){
 					System.out.println("Question:"+q.getQuestion());
 					System.out.println("Outputs : "+search.size()+" "+q.getQuestion());	
@@ -35,9 +41,11 @@ public class TestSet {
 					System.out.println("CA : "+CA);	
 					System.out.println("BingRank : "+bingRank);	
 					amount++;
-					break;
+					isTrue = 1;
+					
 				}
-
+				String[] trainRow = {""+j, bingRank, ""+lse.getScore(), ""+isTrue};
+				csvw.writeNext(trainRow);
 			
 				
 				//System.out.println("BR : "+bingRank+" "+lse.getScore()+" "+q.getQuestion());
@@ -46,6 +54,7 @@ public class TestSet {
 			 System.out.println(" which question: "+j);
 
 		 }
+			bf.close();
 		 System.out.println(" Lucene: "+amount);
 		 System.out.println(" Bing: "+amount);		 
 		}catch (Exception e){
